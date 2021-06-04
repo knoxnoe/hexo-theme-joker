76da095c5d1ac48b94f9535df2bd9399dfcd7176
noe <826547343@qq.com>
2021-06-04 20:12:32 +0800

    feat(_layout css): add comment disqus
    添加了第三方评论插件disqus
    
    BREAKING CHANGE: 加入了评论功能

--- source/js/toc.js
+++ source/js/toc.js
@@ -1,10 +1,19 @@
 import { getPageScroll } from './utils.js'
 
-window.onscroll = function() {
+function setToc() {
   let {y} = getPageScroll();
   let inialTop = 550
   // 500和inialTop的阈值是否添加配置
   if(y <= 500) {
     $("#joker-toc").css("top", inialTop - y)
+  }else {
+    $("#joker-toc").css("top", 50)
   }
 }
+
+function initToc() {
+  setToc()
+}
+
+initToc()
+window.onscroll = setToc;
